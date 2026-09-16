a5a3122ea10e53148b7e907a03cec76ef119c7e5
GEORGE ONYANGO OCHIENG <georgebabji1220@gmail.com>
2026-09-16 18:03:02 +0300

    corrected the low loading of the product

---
+++ find-detailcontent.ps1
@@ -0,0 +1,12 @@
+﻿$path = 'public\html\product-detail.html'
+$lines = Get-Content $path
+for ($i = 0; $i -lt $lines.Length; $i++) {
+    if ($lines[$i] -match 'detailContent') {
+        Write-Host ("Line {0}:" -f ($i + 1))
+        $start = [Math]::Max(0, $i - 1)
+        $end   = [Math]::Min($lines.Length - 1, $i + 2)
+        for ($j = $start; $j -le $end; $j++) {
+            Write-Host ("  [{0}]  {1}" -f ($j + 1), $lines[$j])
+        }
+    }
+}
